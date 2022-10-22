e39f96a18070aec54ef4fdb542dc35116276b3da
Harleen Chahal <harleenchahal@Harleens-MacBook-Pro.local>
2022-10-21 21:44:23 -0400

    without reset

---
+++ src/reset.tsx
@@ -0,0 +1,11 @@
+// import React, { useState } from "react";
+
+// export resetButtons (): JSX.Element {
+//     const [reset, setReset] = useState<number>(0);
+//     return (
+//         <div>
+//         <Button onClick={setReset(0)}
+//         </Button>
+//         </div>
+//     );
+// }
